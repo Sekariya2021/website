bebfa087841ffccd75d98dacdd30a098ae2adb16
Sekariya <85100110+Sekariya2021@users.noreply.github.com>
2022-12-21 09:34:27 +0100

    Testing if login goes through.

--- index.php
+++ index.php
@@ -7,50 +7,18 @@
     <div class="col-md-5">
 </br>
       <h3>Login</h3>
-      <form action="" method="post">
+      <form action="auth.php" method="post">
         <div class="form-group">
           <label>Username</label>
-          <input type="text" class="form-control" name="username" placeholder="Enter username">
+          <input type="text" class="form-control" id ="user" name  = "user" placeholder="Enter username">
         </div>
         <div class="form-group">
           <label>Password</label>
-          <input type="password" class="form-control" name="password" placeholder="Enter password">
+          <input type="password" class="form-control" id ="pass" name  = "pass" placeholder="Enter password">
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary" id="btn">Submit</button>
       </form>
     </div>
   </div>
 </div>
 </body>
-<!-- PHP -->
-<?php
-  if (isset($_POST['username']) && isset($_POST['password'])) 
-  {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    // Connect to database
-    $host = "reservatiesdb.mysql.database.azure.com";
-    $dbUsername = "sekariya";
-    $dbPassword = "Prullenbak123";
-    $dbname = "fonteyn";
-    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
-
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
-    // Query
-    $sql = "SELECT * FROM users WHERE username='".$username."' AND password='".$password."'";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-      // Login success
-      header("Location: dashboard.php");
-    } else {
-      // Login failed
-      echo "Error: Login failed";
-    }
-  }
-?>
