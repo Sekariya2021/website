af67d399bf3b19a51c708bc87df925f2e6712cd9
Sekariya <85100110+Sekariya2021@users.noreply.github.com>
2023-01-13 11:36:54 +0100

    folder

---
+++ www/auth.php
@@ -0,0 +1,45 @@
+
+<?php
+    // connect to database
+    $server = "reservatiesdb.mysql.database.azure.com";
+    $username = "sekariya";
+    $password = "Prullenbak123";
+    $database = "fonteyn";
+
+    // Create connection
+    $conn = new mysqli($server, $username, $password, $database);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // escape variables for security
+    $username = mysqli_real_escape_string($conn, $_POST['user']);
+    $password = mysqli_real_escape_string($conn, $_POST['pass']);
+
+    // check if username exists
+    $sql_check = "SELECT username FROM users WHERE username='$username'";
+    $result = mysqli_query($conn, $sql_check);
+    if (mysqli_num_rows($result) == 0) {
+        echo "This username does not exist. Please try again";
+    } else {
+        // get hashed password from database
+        $sql_check = "SELECT password FROM users WHERE username='$username'";
+        $result = mysqli_query($conn, $sql_check);
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                $hashed_password = $row["password"];
+            }
+        }
+        // check if password is correct
+        if (password_verify($password, $hashed_password)) {
+            header("Location: dashboard.php");
+        } else {
+            echo "Password incorrect. Please try again";
+        }
+    }
+
+    // close connection
+    mysqli_close($conn);
+?>
